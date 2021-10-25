4d4a501157cb4abebb546a8ce69ba2bb1dfd5bc4
MarkZ04 <71509431+MarkZ04@users.noreply.github.com>
2021-10-25 17:37:37 +0300

    create components Header, Menu, Profile

---
+++ socialNetwork/src/components/Menu.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Menu = () => {
+  return (
+    <nav className="menu">
+      <a className="menu-link" href="#">Profile</a>
+      <a className="menu-link" href="#">News</a>
+      <a className="menu-link" href="#">Messages</a>
+      <a className="menu-link" href="#">Music</a>
+      <a className="menu-link" href="#">Settings</a>
+    </nav>
+  )
+}
+
+export { Menu };
---
+++ socialNetwork/src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFC1PJDR0WUeC_LVL1ZFhDcphCVecSwG7q5Q&usqp=CAU"></img>
+      Content
+    </div>
+  )
+}
+
+export { Profile };
